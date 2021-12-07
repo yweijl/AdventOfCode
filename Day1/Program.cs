faa3a4594103a8f0df37460b072d6ad76639edbd
Alexander Weijl <alexanderweij@ad.garmin.com>
2021-12-07 14:22:14 +0100

    refactored day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,67 +1,26 @@
-﻿using System.Net;
+﻿using Shared;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        var uri = new Uri("https://adventofcode.com");
+        using var AdventOfCodeClient = new AdventOfCodeClient();
+        var x = await AdventOfCodeClient.GetAsync("/2021/day/1/input");
+        var list = x.Select(int.Parse).ToList();
 
-        var cookieContainer = new CookieContainer();
-        cookieContainer.Add(uri, new Cookie("_ga", "GA1.2.397973242.1638869066"));
-        cookieContainer.Add(uri, new Cookie("_gid", "GA1.2.752439421.1638869066"));
-        cookieContainer.Add(uri, new Cookie("session", "53616c7465645f5f2d37e6deffc39b18a2529b1475ce5b3c928c676a3333065e0c5cbef58531aa04f310945068940937"));
-
-        using var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
-        using var client = new HttpClient(handler) { BaseAddress = uri };
-
-        var response = await client.GetAsync("/2021/day/1/input");
-        var content = await response.Content.ReadAsStringAsync();
-
-        var input = content.Split("\n").Where(x => !string.IsNullOrEmpty(x)).Select(x => int.Parse(x)).ToArray();
-        var length = input.Count();
-
-
-        var answer = Answer1(input, length);
-        Console.WriteLine($"Answer 1: {answer}");
-
-        answer = Answer2(input, length);
-        Console.WriteLine($"Answer 2: {answer}");
+        Console.WriteLine($"Answer 1: {Assignment1(list)}");
+        Console.WriteLine($"Answer 2: {Assignment2(list)}");
     }
 
-    private static int Answer1(int[] input, int length)
+    private static Func<List<int>, int> occurenceCounter = input
+        => input.Skip(1).Where((item, index) => item > input[index]).Count();
+
+    private static int Assignment1(List<int> list)
+        => occurenceCounter(list);
+
+    private static int Assignment2(List<int> list)
     {
-        var answer = 0;
-        for (int i = 1; i < length; i++)
-        {
-            var previusNumber = input[i - 1];
-            var curentNumber = input[i];
-            if (curentNumber > previusNumber)
-            {
-                answer++;
-            }
-        }
-
-        return answer;
-    }
-
-    private static int Answer2(int[] input, int length){
-        var answer = 0;
-        var previousRange = input.Take(3).Sum();
-
-        for (int i = 1; i < length; i++)
-        {
-            var filterdList = input.Skip(i);
-            if (filterdList.Count() < 3){
-                break;
-            }
-
-            var sum = filterdList.Take(3).Sum();
-            if (sum > previousRange){
-                answer++;
-            }
-            previousRange = sum;
-        }
-
-        return answer;
+        var sums = list.GetRange(0, list.Count -2).Select((item, index) => item + list[index +1] + list[index +2]).ToList();
+        return occurenceCounter(sums);
     }
 }
---
+++ Shared/AdventOfCodeClient.cs
@@ -0,0 +1,36 @@
+﻿namespace Shared;
+
+using System.Net;
+
+public class AdventOfCodeClient : IDisposable
+{
+    private readonly HttpClient _client;
+    private readonly HttpClientHandler _handler;
+
+    public AdventOfCodeClient()
+    {
+        var uri = new Uri("https://adventofcode.com");
+
+        var cookieContainer = new CookieContainer();
+        cookieContainer.Add(uri, new Cookie("_ga", "GA1.2.397973242.1638869066"));
+        cookieContainer.Add(uri, new Cookie("_gid", "GA1.2.752439421.1638869066"));
+        cookieContainer.Add(uri, new Cookie("session", "53616c7465645f5f2d37e6deffc39b18a2529b1475ce5b3c928c676a3333065e0c5cbef58531aa04f310945068940937"));
+
+        _handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+        _client = new HttpClient(_handler) { BaseAddress = uri };
+    }
+
+    public void Dispose()
+    {
+        _client.Dispose();
+        _handler.Dispose();
+        
+    }
+
+    public async Task<IEnumerable<string>> GetAsync(string endpoint){
+        var response = await _client.GetAsync(endpoint);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content.Split("\n").Where(x => !string.IsNullOrEmpty(x));
+    }
+}
