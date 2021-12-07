e54ebf556e816e639f5467ff8ee15161d53bf095
Alexander Weijl <alexanderweij@ad.garmin.com>
2021-12-07 11:54:06 +0100

    Finished day1-1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using System.Net;
+
+var uri = new Uri("https://adventofcode.com");
+
+var cookieContainer = new CookieContainer();
+cookieContainer.Add(uri, new Cookie("_ga", "GA1.2.397973242.1638869066"));
+cookieContainer.Add(uri, new Cookie("_gid", "GA1.2.752439421.1638869066"));
+cookieContainer.Add(uri, new Cookie("session", "53616c7465645f5f2d37e6deffc39b18a2529b1475ce5b3c928c676a3333065e0c5cbef58531aa04f310945068940937"));
+
+using var handler = new HttpClientHandler(){CookieContainer = cookieContainer};
+using var client = new HttpClient(handler){BaseAddress = uri};
+
+var response = await client.GetAsync("/2021/day/1/input");
+var content = await response.Content.ReadAsStringAsync();
+
+var input = content.Split("\n").Where(x => !string.IsNullOrEmpty(x)).ToArray();
+var length = input.Count();
+
+var answer = 0;
+for (int i = 1; i < length; i++){
+    var previusNumber = int.Parse(input[i-1]);
+    var curentNumber = int.Parse(input[i]);
+    if (curentNumber > previusNumber){
+        answer++;
+    }
+}
+
+Console.WriteLine(answer);
