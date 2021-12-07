c22a67d75c1de326b72fbc4318aebdb4e700bbe7
Alexander Weijl <alexanderweij@ad.garmin.com>
2021-12-07 15:46:36 +0100

    Finished day2-2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -5,11 +5,18 @@ public class Program {
         using var client = new AdventOfCodeClient();
         var input = (await client.GetAsync("2021/day/2/input")).ToList();
         Console.WriteLine($"Answer 1 {Assignment1(input)}");
+        Console.WriteLine($"Answer 2 {Assignment2(input)}");
     }
 
     public static int Assignment1(List<string> input){
         var position = new Position();
-        input.ForEach(position.ChangeCourse);
+        input.ForEach(position.ChangeCourseV1);
+        return position.CalculatePosition;
+    }
+
+        public static int Assignment2(List<string> input){
+        var position = new Position();
+        input.ForEach(position.ChangeCourseV2);
         return position.CalculatePosition;
     }
 
@@ -17,21 +24,41 @@ public class Program {
 
 public class Position{
     private int _horizontal = 0;
-    private int _vertical = 0;
+    private int _depth = 0;
+    private int _aim = 0;
 
-    public int CalculatePosition => _horizontal * _vertical;
+    public int CalculatePosition => _horizontal * _depth;
 
-    public void ChangeCourse(string instruction) {
+    public void ChangeCourseV1(string instruction) {
 
         switch(instruction){
             case string x when x.StartsWith("Up", StringComparison.OrdinalIgnoreCase):
-            _vertical -= int.Parse(instruction.Substring(3));
+            _depth -= int.Parse(instruction.Substring(3));
             break;
             case string x when x.StartsWith("Down", StringComparison.OrdinalIgnoreCase):
-            _vertical += int.Parse(instruction.Substring(5));
+            _depth += int.Parse(instruction.Substring(5));
             break;
             case string x when x.StartsWith("Forward", StringComparison.OrdinalIgnoreCase):
             _horizontal += int.Parse(instruction.Substring(8));
+            break;
+            default:
+            throw new ArgumentException(nameof(instruction));
+        }
+    }
+
+        public void ChangeCourseV2(string instruction) {
+
+        switch(instruction){
+            case string x when x.StartsWith("Up", StringComparison.OrdinalIgnoreCase):
+            _aim -= int.Parse(instruction.Substring(3));
+            break;
+            case string x when x.StartsWith("Down", StringComparison.OrdinalIgnoreCase):
+            _aim += int.Parse(instruction.Substring(5));
+            break;
+            case string x when x.StartsWith("Forward", StringComparison.OrdinalIgnoreCase):
+            var amount = int.Parse(instruction.Substring(8));
+            _horizontal += amount;
+            _depth += _aim * amount;
             break;
             default:
             throw new ArgumentException(nameof(instruction));
